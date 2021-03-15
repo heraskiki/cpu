719c4d73a7adcf40c5b224ff27bb50730b1c6012
heraskiki <74484890+heraskiki@users.noreply.github.com>
2021-03-15 21:24:27 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qfztxm27js9gvszpw0tz93clq6pfs86qr93dklh.test1
 
 
 done
